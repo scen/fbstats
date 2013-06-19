dc830629ee88e512010d3aab9624a01e193d09ba
Stanley Cen <jiecenzhao@gmail.com>
2013-06-18 20:42:13 -0700

    Add redirect to /fbstats

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <!-- this is a useless file that allows me to host this static site on heroku -->
 <!-- it is not part of the project. the project files are in the fbstats/ directory -->
+
+<?php header('Location: /fbstats/'); ?>
